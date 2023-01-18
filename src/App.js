2c9b8f03789bafc70152cc79c3b01a6a7ac46289
JBPakula <108825951+JBPakula@users.noreply.github.com>
2023-01-18 11:29:57 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Counter from './Components/Counter/Counter';
+
+function App() {
+
+  return (
+
+    <>
+    <nav></nav>
+    <main>
+      <Counter label="Click on me" size="max"/>
+    </main>
+    <footer></footer>
+    </>
+  );
+}
+
+export default App;
